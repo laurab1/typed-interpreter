7e139ec7f1d8edf8af1c33b374c09d870f220f3e
laurab1 <l.bussi1@studenti.unipi.it>
2025-06-19 10:51:51 +0200

    add more informative errors in typechecker

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -12,41 +12,40 @@ typeCheck env uexpr = case uexpr of
               in case t of
                     Nothing -> Left ("Unbound variable " ++ x)
                     Just ty -> Right ty
-
     EAdd n m -> do
         t1 <- typeCheck env n
         t2 <- typeCheck env m
         case (t1, t2) of
             (TInt, TInt) -> return TInt
-            _ -> Left "Type mismatch"
+            _ -> Left "Type mismatch: add arguments must be integer"
     ESub n m -> do
         t1 <- typeCheck env n
         t2 <- typeCheck env m
         case (t1, t2) of
             (TInt, TInt) -> return TInt
-            _ -> Left "Type mismatch"
+            _ -> Left "Type mismatch: sub arguments must be integer"
     EMul n m -> do
         t1 <- typeCheck env n
         t2 <- typeCheck env m
         case (t1, t2) of
             (TInt, TInt) -> return TInt
-            _ -> Left "Type mismatch"
+            _ -> Left "Type mismatch: mul arguments must be integer"
     EDiv n m -> do
         t1 <- typeCheck env n
         t2 <- typeCheck env m
         case (t1, t2) of
             (TInt, TInt) -> return TInt
-            _ -> Left "Type mismatch"
+            _ -> Left "Type mismatch: div arguments must be integer"
     EIsZero m -> do
         t <- typeCheck env m
-        if t == TInt then Right TBool else Left "Type mismatch"
+        if t == TInt then Right TBool else Left "Type mismatch: iszero argument must be an integer"
     EIf g e1 e2 -> do
-        tg <- typeCheck env g
+        tg <- typeCheck env g 
         t1 <- typeCheck env e1
         t2 <- typeCheck env e2
         case tg of
             TBool -> if t1 == t2 then Right t1 else Left "Type mismatch between if cases"
-            _ -> Left "Type mismatch on boolean guard"
+            _ -> Left "Type mismatch: if guard must be boolean"
     EFun par t body -> do
         t1 <- typeCheck ((par,t):env) body
         return (TFun t t1)
